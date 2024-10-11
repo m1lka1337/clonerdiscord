cf121f8bb4af5a777e34d8b456f976a38d67d954
d3f4ult <167878967+m1lka1337@users.noreply.github.com>
2024-10-11 15:39:23 +0300

    update.pyyy

---
+++ update.py
@@ -0,0 +1,44 @@
+import requests
+import subprocess
+import sys
+from time import sleep
+
+
+class Update():
+    def __init__(self):
+        self.version = '0.2'
+        self.github = 'https://raw.githubusercontent.com/m1lka1337/clonerdiscord/main/update.py'
+        self.zipfile = ''
+        self.update_checker()
+
+    def update_checker(self):
+        code = requests.get(self.github).text
+        if "self.version = '1.8.2'" in code:
+            print('Ваша версия актуальна!')
+            time.sleep(1)
+            pass
+        else:
+            print('''
+                    ███╗   ██╗███████╗██╗    ██╗    ██╗   ██╗██████╗ ██████╗  █████╗ ████████╗███████╗██╗
+                    ████╗  ██║██╔════╝██║    ██║    ██║   ██║██╔══██╗██╔══██╗██╔══██╗╚══██╔══╝██╔════╝██║
+                    ██╔██╗ ██║█████╗  ██║ █╗ ██║    ██║   ██║██████╔╝██║  ██║███████║   ██║   █████╗  ██║
+                    ██║╚██╗██║██╔══╝  ██║███╗██║    ██║   ██║██╔═══╝ ██║  ██║██╔══██║   ██║   ██╔══╝  ╚═╝
+                    ██║ ╚████║███████╗╚███╔███╔╝    ╚██████╔╝██║     ██████╔╝██║  ██║   ██║   ███████╗██╗
+                    ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝      ╚═════╝ ╚═╝     ╚═════╝ ╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝
+                                      Ваша версия устарела выберите вариант ниже''')
+            choice = input('\nХотите обновить? (да/нет): ')
+            if choice.lower() == 'да':
+                print('Скачивание обновления...')
+                new_version_source = requests.get(self.zipfile)
+                with open("Update.zip", 'wb')as zipfile:
+                    zipfile.write(new_version_source.content)
+
+                subprocess.Popen([sys.executable, 'updater.py'])
+
+                sys.exit(2)
+            if choice.lower() == 'нет':
+                sys.exit(0)
+
+
+if __name__ == '__main__':
+    Update()
